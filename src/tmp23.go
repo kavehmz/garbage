0d8958c0d2a6e05dc858d7cfe299ae898f2e63df
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-02-12 21:18:52 +0800

    dump

--- src/tmp23.go
+++ src/tmp23.go
@@ -1,46 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"io"
-)
-import "github.com/kavehmz/enoox"
+import "fmt"
 
-type setupMessage struct {
-	ready   bool
-	message string
+type t struct {
+	m string
 }
 
-// func setupUiServer(dbReady chan bool) {
-// 	ins := createDigitalOcteanInstance(myUISetup)
-// 	dbState := <-dbReady
-// 	if dbState {
-// 		bootInstace(ins)
-// 	}
-// }
-//
-// func setupClientDB(dbReady chan string) {
-// 	ins := createDigitalOcteanInstance(myDBSetup)
-// 	bootInstace(ins)
-// 	initDBData(ins)
-// 	if checkDBState() {
-// 		dbReady <- true
-// 		return
-// 	}
-// 	dbReady <- false
-// 	return
-// }
-
-func aa(aa enoox.Server) {
-	io.Writer
-	return
+func (a t) aa() {
+	fmt.Println("test")
 }
 
 func main() {
-	fmt.Println()
-	var a enoox.RectAA
-	fmt.Println(a)
-	// com := make(chan string, 0)
-	// go setupUiServer(com)
-	// go setupClientDB(com)
+	t{m: "test"}.aa()
 }
