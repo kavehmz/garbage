3174f622857b4da33de41cd3a6be8a838774c2ce
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-02-27 10:32:55 +0800

    utf8

--- tmp31.go
+++ tmp31.go
@@ -5,6 +5,23 @@ import (
 	"fmt"
 )
 
+// Δ is upper case so it needs some comments!
+func Δ(x, y int) int {
+	return x - y
+}
+
+func چاپ() {
+	fmt.Println("آروین")
+}
+
+func 인쇄() {
+	fmt.Println("실험")
+}
+
+func 打印() {
+	fmt.Println("實驗")
+}
+
 func main() {
 	var data = []byte(`{"status": 200, "name":"test", "t":{"a":"b"}}`)
 
@@ -18,4 +35,10 @@ func main() {
 	fmt.Println("status value:", status)
 
 	fmt.Println("status value:", result["t"].(map[string]interface{})["a"])
+
+	fmt.Println(Δ(8, 2))
+	چاپ()
+	인쇄()
+	打印()
+
 }
