3ed002b744b3805c6f335e9f98714e1ee4be514d
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-03-03 19:53:41 +0800

    shabang run in commandline

---
+++ tmp36.go
@@ -0,0 +1,19 @@
+//usr/bin/env go run $0 $@; exit $?
+package main
+
+import "time"
+
+func init() { print("A\n") }
+func f() {
+	defer func() {
+		print("on exit\n")
+	}()
+	print("bye\n")
+}
+
+func init() { print("B\n") }
+func main() {
+	go f()
+
+	time.Sleep(time.Second)
+}
