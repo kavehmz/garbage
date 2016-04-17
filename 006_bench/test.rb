fddc22ce8d9d5f5548826090897dd2aa65998822
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-04-17 21:38:51 +0800

    just to be fair

--- 006_bench/test.rb
+++ 006_bench/test.rb
@@ -6,29 +6,29 @@ Contract.new()
 
 t0 = Time.now
 n=0;
-for i in 0..10000000
+for i in 1..10000000
 	n+=1
 end
 puts "loop time :" + (Time.now- t0).to_s
 
 t0 = Time.now
 n=0;
-for i in 0..10000000
+for i in 1..10000000
 	n=Math.exp(i)
 end
 puts "exp time :"+  (Time.now- t0).to_s
 
 t0 = Time.now
-for i in  0..10000
+for i in  1..10000
 	a=Array.new
-	for j in  0..10000
+	for j in  1..10000
 		a.push(j)
 	end
 end
 puts "append time :"+  (Time.now- t0).to_s
 
 t0 = Time.now
-for i in  0..10000000
+for i in  1..10000000
 	p=Contract.new;
 end
 puts "class time :"+  (Time.now- t0).to_s
