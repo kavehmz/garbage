2cd503f46faf06492711f3c7add3819a76a8d1c8
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-04-17 20:14:51 +0800

    Adding Ruby to the bunch and adding README

---
+++ 006_bench/README.md
@@ -0,0 +1,20 @@
+Just a simple comparison for few operation in different languages.
+
+```bash
+echo "Go";go run test.go; echo "Perl";perl test.pl;echo "Ruby";ruby test.rb
+Go
+loop time   : 0.002945188
+loop time   : 0.319440381
+append time : 0.566224212
+struct time   : 0.003823304
+Perl
+loop time :0.706536
+exp time :1.118389
+append time :8.218233
+package time :5.25964
+Ruby
+loop time :0.556671
+exp time :3.305478
+append time :9.424143
+package time :1.710611
+```
---
+++ 006_bench/test.rb
@@ -0,0 +1,34 @@
+
+class Contract
+end
+
+Contract.new()
+
+t0 = Time.now
+n=0;
+for i in 0..10000000
+	n+=1
+end
+puts "loop time :" + (Time.now- t0).to_s
+
+t0 = Time.now
+n=0;
+for i in 0..10000000
+	n=Math.exp(i)
+end
+puts "exp time :"+  (Time.now- t0).to_s
+
+t0 = Time.now
+for i in  0..10000
+	a=Array.new
+	for j in  0..10000
+		a.push(j)
+	end
+end
+puts "append time :"+  (Time.now- t0).to_s
+
+t0 = Time.now
+for i in  0..10000000
+	p=Contract.new;
+end
+puts "package time :"+  (Time.now- t0).to_s
