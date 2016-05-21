30b9b4fd024860dca8f4e25c3ea2e934f5666959
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-05-21 14:18:40 +0800

    more stress

--- 007_stress/binary/binary_ws.go
+++ 007_stress/binary/binary_ws.go
@@ -8,17 +8,20 @@ import (
 	"golang.org/x/net/websocket"
 )
 
+// var req = `{
+// "proposal": 1,
+// "amount": "100",
+// "basis": "payout",
+// "contract_type": "CALL",
+// "currency": "USD",
+// "duration": "60",
+// "duration_unit": "s",
+// "symbol": "R_100"
+// }`
+
 var req = `
 {
-  "symbol": "frxAUDUSD",
-  "subscribe": 1,
-  "duration": 2,
-  "currency": "USD",
-  "amount": 10,
-  "proposal": 1,
-  "duration_unit": "m",
-  "basis": "payout",
-  "contract_type": "PUT"
+	"website_status": 1
 }`
 
 func chk(e error) {
@@ -28,24 +31,29 @@ func chk(e error) {
 }
 
 func main() {
-	for i := 0; i < 235; i++ {
+	for i := 0; i < 2000; i++ {
 		go func(num int) {
 			t := time.Now().UnixNano()
-			ws, err := websocket.Dial("wss://www2.binary.com/websockets/v3?l=EN&debug=1", "", "http://localhost/")
+			ws, err := websocket.Dial("wss://ws.binaryws.com/websockets/v3?app_id=1&l=EN", "", "http://load_test")
 			chk(err)
 			fmt.Println("Time", num, ":", float64(time.Now().UnixNano()-t)/1000000000)
 
-			for j := 0; j < 10; j++ {
+			for j := 0; j < 3000; j++ {
 				t = time.Now().UnixNano()
 				_, err = ws.Write([]byte(req))
 				chk(err)
 				var msg = make([]byte, 5120)
-				_, err = ws.Read(msg)
+				_, err := ws.Read(msg)
 				chk(err)
-				fmt.Printf("Received: %d after %f\n", num, float64(time.Now().UnixNano()-t)/1000000000)
+				// fmt.Println(string(msg))
+				if j%10 == 0 {
+					ws, _ = websocket.Dial("wss://ws.binaryws.com/websockets/v3?app_id=1&l=EN", "", "http://load_test")
+					fmt.Printf("[%d, %d] Received for : %d after %f\n", j, num, num, float64(time.Now().UnixNano()-t)/1000000000)
+				}
+				time.Sleep(time.Millisecond * 1100)
 			}
 		}(i)
 	}
 
-	time.Sleep(time.Second * 50)
+	time.Sleep(time.Second * 10000)
 }
