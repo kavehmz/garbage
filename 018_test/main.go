356c5f308541a79042ec17b80a3020de7f774701
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-11-16 22:49:16 +0100

    pub

--- 018_test/main.go
+++ 018_test/main.go
@@ -6,32 +6,40 @@ import (
 	"github.com/streadway/amqp"
 )
 
-func checkErr(err error) {
+func failOnError(err error, msg string) {
 	if err != nil {
-		log.Panic(err)
+		log.Fatalf("%s: %s", msg, err)
 	}
 }
 
-var processBuff = make(chan bool, 4)
-
-func publish() {
+func main() {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:15672/")
-	checkErr(err)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
 
 	ch, err := conn.Channel()
-	checkErr(err)
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
 
-	err = ch.Publish(
-		"LiveFaresLog", // exchange
-		"Error",        // routing key
-		false,          // mandatory
-		false,          // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte("test_body"),
-		})
-}
+	msgs, err := ch.Consume(
+		"LiveFaresLog_Error", // queue
+		"",                   // consumer
+		true,                 // auto-ack
+		false,                // exclusive
+		false,                // no-local
+		false,                // no-wait
+		nil,                  // args
+	)
+	failOnError(err, "Failed to register a consumer")
 
-func main() {
-	publish()
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
