8a535eb385ccb04cb1f2cd08d924422de40ca342
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2017-01-05 09:24:46 +0100

    stripping the extra

--- 026_knockd_with_temp_ports/refresh_knock.sh
+++ 026_knockd_with_temp_ports/refresh_knock.sh
@@ -7,9 +7,9 @@ cat <<EOF > /etc/knockd.conf
     sequence = $1
     tcpflags = syn
     seq_timeout = 5
-    start_command = /sbin/iptables -I office_access_from_live 1 -s %IP% -p tcp --dport 22 -j ACCEPT
+    start_command = /sbin/iptables -I ssh_access 1 -s %IP% -p tcp --dport 22 -j ACCEPT
     cmd_timeout = 20
-    stop_command = /sbin/iptables -D office_access_from_live -s %IP% -p tcp --dport 22 -j ACCEPT
+    stop_command = /sbin/iptables -D ssh_access -s %IP% -p tcp --dport 22 -j ACCEPT
 EOF
 }
 
@@ -18,17 +18,17 @@ do
     EPOCH=$(date +%s)
     if [ $(($EPOCH % 30)) == "0" ]
     then
-        CODE=$(cat /etc/rmg/knockd.code.01)
+        CODE=$(cat /etc/knockd/knockd.code.01)
         SEQ1=$(oathtool --base32 --totp "$CODE" -d 8|sed -e 's/\(....\)\(....\)/\1,\2/g')
-        CODE=$(cat /etc/rmg/knockd02.code.02)
+        CODE=$(cat /etc/knockd/knockd02.code.02)
         SEQ2=$(oathtool --base32 --totp "$CODE" -d 8|sed -e 's/\(....\)\(....\)/\1,\2/g')
         write_knockd_conf $SEQ1,$SEQ2
-        iptables -F office_access_from_live
+        iptables -F ssh_access
         service knockd reload
     fi
     if [ $(($EPOCH % 600)) == "0" ]
     then
-        aws s3 sync s3://knockd/ /etc/rmg/
+        aws s3 sync s3://knockd/ /etc/knockd/
     fi
     sleep 1
 done
