b93216c5c598130ac61486e8026fd8eeabc80210
Kaveh Mousavi Zamani <kaveh@fitanalytics.com>
2019-03-24 08:41:42 +0100

    delete the untidy ones

--- 001_dump/a.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import . "fmt"
-
-func main() {
-	Println("Go home!"[0:1])
-	Println(string([]rune("🏠🚴")[0:1]))
-	Println(uint("🏠🚴"[0:1]))
-}
--- 001_dump/atkin/atkin_test.go
+++
@@ -1,9 +0,0 @@
-package atkin
-
-import "testing"
-
-func BenchmarkPrimes(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Atkin(100000000)
-	}
-}
