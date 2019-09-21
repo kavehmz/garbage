4bf6461debdc8d09d80a969b363e59c1c0d7cb93
Kaveh Mousavi Zamani <kaveh@fitanalytics.com>
2019-09-21 12:30:36 +0200

    longer one

--- 054_k&r/detab.c
+++ 054_k&r/detab.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 
-#define TAB_LENGTH 4
-#define MAX_LENGTH 1000
-
+void process_input(int max, int tab_stop);
 int append_space(char *base, int at,int tab_size);
 int append_char(char *base,int at, char c);
+short append_fits(int pos,int max, int tab_top, char c);
+
 
 int main() {
-    char c;
-    char line[MAX_LENGTH];
-    int pos=0;
-
-    while ((c=getchar())!=EOF) {
-        if (c!='\t') {
-            pos=append_char(line,pos,c);
-        } else {
-            pos=append_space(line,pos, TAB_LENGTH);
-        }
-    }
-    append_char(line,pos,'\0');
-    printf("%s", line);
+    process_input(7,4);
     return 0;
 }
 
-int append_space(char *line, int at,int tab_size) {
+void process_input(int max, int tab_stop) {
+    char c;
+    char line[max];
+    int pos=0;
+
+    while ((c=getchar())!=EOF && append_fits(pos,max,tab_stop,c) ) {
+        if (c=='\t') {
+            pos=append_space(line,pos, tab_stop);
+        } else {
+            pos=append_char(line,pos,c);
+        }
+    }
+    append_char(line,pos,'\0');
+    printf("[%s]", line);
+}
+
+int append_space(char *line, int at, int tab_size) {
     for (int i=0;i<tab_size;i++){
         at=append_char(line,at,' ');
     }
@@ -34,3 +38,16 @@ int append_char(char *line,int at, char c) {
     line[at]=c;
     return at+1;
 }
+
+short append_fits(int pos, int max, int tab_stop, char c) {
+    max=max-1; // save room for \0
+    int size=1;
+    if (c=='\t') {
+        size=tab_stop;
+    }
+    if (pos>=max || c=='\t' && pos+size>=max) {
+        return 0;
+    }
+    return 1;
+}
+
