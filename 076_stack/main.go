a5d94f4e9ea229a7fa82260bf0caa86fd4b30ec7
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2021-09-06 19:19:07 +0200

    by value first call: 19.938792ms by value second call: 11.558083ms by value no large use: 10.31675ms by ref first call: 1.542µs by ref second call: 958ns

---
+++ 076_stack/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const AN = 10000000
+const BN = 1000
+
+func byBalue(a largeStruct) {
+	b := [BN]int{}
+
+	a.x = a.d[0]
+	a.y = a.d[1]
+	for i := 1; i < BN; i++ {
+		b[i] = b[i] + a.x + a.y + i
+	}
+}
+
+// checking if compile does any optimizatios
+func byBalueNotUseingTheLargeArray(a largeStruct) {
+	b := [BN]int{}
+
+	for i := 1; i < BN; i++ {
+		b[i] = b[i] + a.x + a.y + i
+	}
+}
+
+func byRef(a *largeStruct) {
+	b := [BN]int{}
+
+	for i := 1; i < BN; i++ {
+		b[i] = b[i] + a.d[i-1] + i
+	}
+}
+
+type largeStruct struct {
+	d [AN]int
+	x int
+	y int
+}
+
+func main() {
+
+	ls := largeStruct{}
+
+	start := time.Now()
+	byBalue(ls)
+	fmt.Println("by value first call:", time.Since(start))
+
+	start = time.Now()
+	byBalue(ls)
+	fmt.Println("by value second call:", time.Since(start))
+
+	start = time.Now()
+	byBalueNotUseingTheLargeArray(ls)
+	fmt.Println("by value no large use:", time.Since(start))
+
+	start = time.Now()
+	byRef(&ls)
+	fmt.Println("by ref first call:", time.Since(start))
+
+	start = time.Now()
+	byRef(&ls)
+	fmt.Println("by ref second call:", time.Since(start))
+}
